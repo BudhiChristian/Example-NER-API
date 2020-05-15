79e931f78d247012fae34b3ee978c59aecdfd60f
BudhiChristian <cjbudhi95@gmail.com>
2020-05-15 17:53:15 -0400

    Complete data prep to spacy format

--- app/training_module/controller.py
+++ app/training_module/controller.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, request
 
-from .data_prep import csv_to_json
+from .data_prep import csv_to_spacy
 
 training_controller = Blueprint('training', __name__, url_prefix='/training')
 
 @training_controller.route('/trigger', methods=['POST'])
 def trigger_training():
-    training_data = csv_to_json()
-    return training_data
+    training_data = csv_to_spacy()
+    return 'hello world'
--- app/training_module/data_prep.py
+++ app/training_module/data_prep.py
@@ -2,17 +2,9 @@ import csv
 
 from . import csv_location
 
-'''
-0 sentence #
-1 word
-2 pos
-3 tag
-
-'''
-def csv_to_json():
+def csv_to_spacy():
     csv_data = __read_csv_file()
-    return 'test'
-
+    spacy_data = __convert_to_spacy(csv_data)
 
 def __read_csv_file():
     data = list()
@@ -20,9 +12,38 @@ def __read_csv_file():
         reader = csv.reader(csv_file)
         for line in reader:
             data.append(line)
-    return data
+    return data[1:]
 
-def __convert_to_json(csv_data):
+def __convert_to_spacy(csv_data):
+    '''
+    csv column ref
+    0 sentence #
+    1 word
+    2 pos
+    3 tag
+    '''
     training_data = list()
-    for line in csv_data:
-        pass
+
+    line = list()
+    entities = list()
+    current_index = 0
+    for data in csv_data:
+        if data[0] and len(line) > 0:
+            data_entry = (' '.join(line), { "entities": entities })
+            training_data.append(data_entry)
+            line = list()
+            entities = list()
+            current_index = 0
+        line.append(data[1])
+        if data[3] != 'O':
+            entity = (
+                current_index, 
+                current_index+len(data[1]), 
+                data[3])
+            entities.append(entity)
+        current_index += (len(data[1])+1)
+    return training_data
+        
+        
+        
+        
