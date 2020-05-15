ceee0378a0a9588bc5fa2705a3e069d9bee4a246
BudhiChristian <cjbudhi95@gmail.com>
2020-05-15 18:45:23 -0400

    Updating status

--- app/training_module/training.py
+++ app/training_module/training.py
@@ -1,9 +1,11 @@
+import math
 import random
 import spacy
 from spacy.util import minibatch
 
 epochs = 10
 batch_size = 10
+progress_size = 40
 
 def train_model(training_data, labels):
     model = spacy.blank('en')
@@ -25,8 +27,21 @@ def train_model(training_data, labels):
             batches = minibatch(training_data, size=batch_size)
 
             for idx, batch in enumerate(batches):
-                print(f"{idx*batch_size}/{len(training_data)}\r", end="")
                 texts, annotations = zip(*batch)
                 model.update(texts, annotations, sgd=optimizer, drop=0.35, losses=losses)
-            print(f"{len(training_data)}/{len(training_data)}")
-            print('Losses', losses)
+                __print_progress((idx*batch_size)+len(batch), len(training_data))
+            print('\nLosses', losses)
+
+def __print_progress(current, total):
+    num_complete = math.floor(progress_size * (current/total))
+    num_remaining = progress_size - num_complete
+
+
+    progress = ""
+    if num_complete > 0:
+        progress = ("=" * (num_complete - 1))+">"
+    remaining = "_" * num_remaining
+
+    print(f"{current}/{total} complete [{progress}{remaining}]\r", end="")
+    
+
